d8365f1ea54c6b6afd9d605961237b3cd860ccfa
Pavel <verccety1@gmail.com>
2021-01-05 21:04:03 +0700

    Implemented Sign-up with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,32 @@ const config = {
   appId: "1:731060121249:web:264879f1ca494d610b4ae0"
 }
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+ if (!userAuth) return
+
+ const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+ const snapShot = await userRef.get() // через снапшот определяем exists ли объект, snapshot simply represents the data
+
+//если данных о польз. нету, то создаем запись в ДБ
+ if (!snapShot.exists) { 
+  const {displayName, email} = userAuth
+  const createdAt = new Date()
+
+  try {
+    await userRef.set({
+      displayName,
+      email,
+      createdAt,
+      ...additionalData
+    })
+  } catch (error) {
+    console.log('error creating user', error.message);
+  }
+ }
+ return userRef
+}
+
 firebase.initializeApp(config)
 
 export const auth = firebase.auth() 
