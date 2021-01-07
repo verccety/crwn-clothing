a023254838a14d9154f18d910ff3086f203dc659
Pavel <verccety1@gmail.com>
2021-01-07 22:37:49 +0700

    Added memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-import  CustomButton  from "../custom-button/custom-button.component";
-import CartItem from "../cart-item/cart-item.component";
+import CustomButton from '../custom-button/custom-button.component'
+import CartItem from '../cart-item/cart-item.component'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors'
+
 import './cart-dropdown.styles.scss'
 import { connect } from 'react-redux'
 
-
-const CartDropdown = ({cartItems}) => {
+const CartDropdown = ({ cartItems }) => {
   return (
     <div className='cart-dropdown'>
-    <div className="cart-items">{
-      cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem}/>)
-    }</div>
+      <div className='cart-items'>
+        {cartItems.map((cartItem) => (
+          <CartItem key={cartItem.id} item={cartItem} />
+        ))}
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   )
 }
-const mapStateToProps = ({cart: {cartItems}}) => ({
-  cartItems 
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItemsCount(state),
 })
 export default connect(mapStateToProps)(CartDropdown)
