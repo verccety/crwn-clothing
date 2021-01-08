19ae9b86f5febb1e85482782ed6ff809f08e167b
Pavel <verccety1@gmail.com>
2021-01-08 17:25:06 +0700

    Added redux-persist

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import {Provider} from 'react-redux'
-import store from './redux/store'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App'
+import { Provider } from 'react-redux'
+import {store, persistor} from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
 
 ReactDOM.render(
   <React.StrictMode>
-  <Provider store={store} >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <App />
+        </PersistGate>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
-);
+  document.getElementById('root')
+)
