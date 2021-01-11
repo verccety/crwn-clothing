47341ddfc22bf1c0c31d63dc544b430e2f0ca832
Pavel <verccety1@gmail.com>
2021-01-11 13:44:13 +0700

    Moving SHOP_DATA to FireBase

--- src/App.js
+++ src/App.js
@@ -4,21 +4,26 @@ import { createStructuredSelector } from 'reselect';
 import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/header/header.component';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument,
+  addCollectionAndDocuments,
+} from './firebase/firebase.utils';
 import CheckoutPage from './pages/checkout/checkout.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentuser } from './redux/user/user.selector';
+import { selectCollectionsForPreview } from './redux/shop/shop.selectors';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
-    // параметр userAuth текущая регист. инф. о польз.
+    const { setCurrentUser, collectionsArray } = this.props;
 
+    // параметр userAuth текущая регист. инф. о польз.
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
@@ -29,9 +34,13 @@ class App extends React.Component {
             ...snapShot.data(),
           });
         });
-      } else {
-        setCurrentUser(userAuth);
       }
+      setCurrentUser(userAuth);
+      addCollectionAndDocuments(
+        'collections',
+        // нужен не весь объект, а только title, items
+        collectionsArray.map(({ title, items }) => ({ title, items })),
+      );
     });
   }
 
@@ -59,6 +68,7 @@ class App extends React.Component {
 }
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentuser,
+  collectionsArray: selectCollectionsForPreview,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,8 @@ import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
 
 const middlewares = [];
-
-if (process.env.NODE_ENV === 'development') middlewares.push(logger);
+// middleware работает только в режиме development
+if (process.env.NODE_ENV === 'development') middlewares.push(logger); 
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
