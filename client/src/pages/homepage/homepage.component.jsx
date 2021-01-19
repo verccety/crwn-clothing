6c4651a288074c67d0f43038ac7bed5e5b0bc5fa
Pavel <verccety1@gmail.com>
2021-01-19 18:08:04 +0700

    Measure component perfomance with Profiler

--- client/src/pages/homepage/homepage.component.jsx
+++ client/src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Profiler } from 'react';
 import Directory from '../../components/directory/directory.component';
 import { HomePageContainer } from './homepage.styles.jsx';
 const HomePage = () => {
-
-  
   return (
     <HomePageContainer>
-      <Directory />
+      <Profiler
+        id='Directory'
+        onRender={(id, phase, duration) => {console.log({ id, phase, duration })}}>
+        <Directory />
+      </Profiler>
     </HomePageContainer>
   );
 };
